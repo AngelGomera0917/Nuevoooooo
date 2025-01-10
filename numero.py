c219ea9fa0de58a112f803304df920d8a159cdbe
Angel Antonio Gomera <antoniogomera12@gmail.com>
2025-01-10 00:40:43 -0600

    Funcion par y impar

---
+++ numero.py
@@ -0,0 +1,15 @@
+# Esto me dira si un numero es par o impar
+
+print("\n")
+
+def par (valor):
+    return valor % 2 == 0
+# El modulo del valor ingresado debe ser 0
+print(par (4))
+
+def impar (valor):
+    return valor % 2 != 0
+# Su modulo debe dar 1
+
+print(impar (7))
+
